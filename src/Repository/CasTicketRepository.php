4e118344cd42b8ddd74b8b6c7aafada6f5ad4f2e
Dustin Scarberry <dustin@codeclouds.net>
2020-05-18 12:34:39 -0400

    Symfony 4.4

--- src/Repository/CasTicketRepository.php
+++ src/Repository/CasTicketRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\CasTicket;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * @method CasTicket|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class CasTicketRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, CasTicket::class);
     }
--- src/Repository/ServiceProviderRepository.php
+++ src/Repository/ServiceProviderRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\ServiceProvider;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * @method ServiceProvider|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class ServiceProviderRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
       parent::__construct($registry, ServiceProvider::class);
     }
