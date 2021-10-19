cd8e6245cdbff86194718e55f52693573893455c
Dustin Scarberry <bitsnbytes1001@gmail.com>
2021-10-19 11:34:52 -0400

    App root redirect

---
+++ src/Controller/View/App/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller\View\App;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DefaultController extends AbstractController
+{
+  /**
+   * @Route("/")
+   */
+  public function siteRoot()
+  {
+    return $this->redirect($_ENV['APP_ESCAPE_URL']);
+  }
+}
