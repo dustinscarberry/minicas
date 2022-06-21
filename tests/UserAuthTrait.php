092c816ce4dcbc1966eb4ae5fa00d7ed743de552
Dustin Scarberry <bitsnbytes1001@gmail.com>
2022-06-21 11:26:33 -0400

    Testing

--- tests/UserAuthTrait.php
+++ tests/UserAuthTrait.php
@@ -19,26 +19,5 @@ trait UserAuthTrait
 
     // simulate $testUser being logged in
     $client->loginUser($testUser);
-
-
-
-  //  $session = self::$container->get('session');
-
-    // fetch user
-  //  $user = self::$container->get('doctrine')->getRepository(User::class)->findOneBy(['username' => $username]);
-
-//    $firewallName = 'main';
-    // if you don't define multiple connected firewalls, the context defaults to the firewall name
-    // See https://symfony.com/doc/current/reference/configuration/security.html#firewall-context
-//    $firewallContext = 'main';
-
-    // you may need to use a different token class depending on your application.
-    // for example, when using Guard authentication you must instantiate PostAuthenticationGuardToken
-  //  $token = new UsernamePasswordToken($user, null, $firewallName, $user->getRoles());
-  //  $session->set('_security_' . $firewallContext, serialize($token));
-  //  $session->save();
-//
-//    $cookie = new Cookie($session->getName(), $session->getId());
-  //  $this->client->getCookieJar()->set($cookie);
   }
 }
