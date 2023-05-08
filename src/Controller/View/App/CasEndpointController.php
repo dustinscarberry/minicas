2d80e4e90cd8de2e8a50d5255854678bc809c67b
Dustin Scarberry <bitsnbytes1001@gmail.com>
2023-05-08 12:09:30 -0400

    Missing use statement

--- src/Controller/View/App/CasEndpointController.php
+++ src/Controller/View/App/CasEndpointController.php
@@ -4,6 +4,7 @@ namespace App\Controller\View\App;
 
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\HeaderUtils;
