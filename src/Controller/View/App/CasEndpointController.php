442045fb42fffd8564d54d7b5d519aff16419310
Dustin Scarberry <bitsnbytes1001@gmail.com>
2023-05-19 10:21:22 -0400

    Code cleanup

--- src/Controller/View/App/CasEndpointController.php
+++ src/Controller/View/App/CasEndpointController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller\View\App;
 
-
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
--- src/Controller/View/App/DefaultController.php
+++ src/Controller/View/App/DefaultController.php
@@ -2,8 +2,6 @@
 
 namespace App\Controller\View\App;
 
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
